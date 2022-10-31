c53a2a1cd511bbb4d06806a02dd73620f7238112
Rui Carmo <rui.carmo@gmail.com>
2022-10-31 07:58:02 +0000

    change peer detection logic to use local IP address

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-var process = require('process')
+const process = require('process')
 // Handle SIGINT
 process.on('SIGINT', () => {
-  console.info("SIGINT Received, exiting...")
-  process.exit(0)
+    console.info("SIGINT Received, exiting...")
+    process.exit(0)
 })
 
 // Handle SIGTERM
 process.on('SIGTERM', () => {
-  console.info("SIGTERM Received, exiting...")
-  process.exit(0)
+    console.info("SIGTERM Received, exiting...")
+    process.exit(0)
 })
 
 const parser = require('ua-parser-js');
@@ -16,12 +16,24 @@ const { uniqueNamesGenerator, animals, colors } = require('unique-names-generato
 
 class SnapdropServer {
 
-    constructor(port) {
+    constructor(port, mode) {
+        const express = require('express')
         const WebSocket = require('ws');
-        this._wss = new WebSocket.Server({ port: port });
+        this._app = express();
+        this._app.use(express.static('client'))
+        this._wss = new WebSocket.Server({ noServer: true });
         this._wss.on('connection', (socket, request) => this._onConnection(new Peer(socket, request)));
         this._wss.on('headers', (headers, response) => this._onHeaders(headers, response));
 
+        const server = this._app.listen(port);
+
+        server.on('upgrade', (request, socket, head) => {
+            this._wss.handleUpgrade(request, socket, head, socket => {
+                this._wss.emit('connection', socket, request);
+            });
+        });
+
+        this._lan = mode;
         this._rooms = {};
 
         console.log('Snapdrop is running on port', port);
@@ -30,6 +42,7 @@ class SnapdropServer {
     _onConnection(peer) {
         this._joinRoom(peer);
         peer.socket.on('message', message => this._onMessage(peer, message));
+        peer.socket.on('error', console.error);
         this._keepAlive(peer);
 
         // send displayName
@@ -40,18 +53,21 @@ class SnapdropServer {
                 deviceName: peer.name.deviceName
             }
         });
+        console.log("New Peer: " + peer.name.displayName)
     }
 
     _onHeaders(headers, response) {
         if (response.headers.cookie && response.headers.cookie.indexOf('peerid=') > -1) return;
         response.peerId = Peer.uuid();
-        headers.push('Set-Cookie: peerid=' + response.peerId + "; SameSite=Strict; Secure");
+        headers.push('Set-Cookie: peerid=' + response.peerId + "; SameSite=Strict;");
     }
 
     _onMessage(sender, message) {
+
         // Try to parse message 
         try {
             message = JSON.parse(message);
+            console.log(sender.name.displayName + " -> " + message["type"])
         } catch (e) {
             return; // TODO: handle malformed JSON
         }
@@ -65,10 +81,15 @@ class SnapdropServer {
                 break;
         }
 
+
+        var roomKey = "local"
+        if (!this._lan) {
+            roomKey = sender.ip
+        }
         // relay message to recipient
-        if (message.to && this._rooms[sender.ip]) {
+        if (message.to && this._rooms[roomKey]) {
             const recipientId = message.to; // TODO: sanitize
-            const recipient = this._rooms[sender.ip][recipientId];
+            const recipient = this._rooms[roomKey][recipientId];
             delete message.to;
             // add sender id
             message.sender = sender.id;
@@ -78,14 +99,20 @@ class SnapdropServer {
     }
 
     _joinRoom(peer) {
+        var roomKey = "local"
+        if (!this._lan) {
+            roomKey = peer.ip
+        }
+
+
         // if room doesn't exist, create it
-        if (!this._rooms[peer.ip]) {
-            this._rooms[peer.ip] = {};
+        if (!this._rooms[roomKey]) {
+            this._rooms[roomKey] = {};
         }
 
         // notify all other peers
-        for (const otherPeerId in this._rooms[peer.ip]) {
-            const otherPeer = this._rooms[peer.ip][otherPeerId];
+        for (const otherPeerId in this._rooms[roomKey]) {
+            const otherPeer = this._rooms[roomKey][otherPeerId];
             this._send(otherPeer, {
                 type: 'peer-joined',
                 peer: peer.getInfo()
@@ -94,8 +121,8 @@ class SnapdropServer {
 
         // notify peer about the other peers
         const otherPeers = [];
-        for (const otherPeerId in this._rooms[peer.ip]) {
-            otherPeers.push(this._rooms[peer.ip][otherPeerId].getInfo());
+        for (const otherPeerId in this._rooms[roomKey]) {
+            otherPeers.push(this._rooms[roomKey][otherPeerId].getInfo());
         }
 
         this._send(peer, {
@@ -104,24 +131,29 @@ class SnapdropServer {
         });
 
         // add peer to room
-        this._rooms[peer.ip][peer.id] = peer;
+        this._rooms[roomKey][peer.id] = peer;
     }
 
     _leaveRoom(peer) {
-        if (!this._rooms[peer.ip] || !this._rooms[peer.ip][peer.id]) return;
-        this._cancelKeepAlive(this._rooms[peer.ip][peer.id]);
+        var roomKey = "local"
+        if (!this._lan) {
+            roomKey = peer.ip
+        }
+
+        if (!this._rooms[roomKey] || !this._rooms[roomKey][peer.id]) return;
+        this._cancelKeepAlive(this._rooms[roomKey][peer.id]);
 
         // delete the peer
-        delete this._rooms[peer.ip][peer.id];
+        delete this._rooms[roomKey][peer.id];
 
         peer.socket.terminate();
         //if room is empty, delete the room
-        if (!Object.keys(this._rooms[peer.ip]).length) {
-            delete this._rooms[peer.ip];
+        if (!Object.keys(this._rooms[roomKey]).length) {
+            delete this._rooms[roomKey];
         } else {
             // notify all other peers
-            for (const otherPeerId in this._rooms[peer.ip]) {
-                const otherPeer = this._rooms[peer.ip][otherPeerId];
+            for (const otherPeerId in this._rooms[roomKey]) {
+                const otherPeer = this._rooms[roomKey][otherPeerId];
                 this._send(otherPeer, { type: 'peer-left', peerId: peer.id });
             }
         }
@@ -165,7 +197,6 @@ class Peer {
         // set socket
         this.socket = socket;
 
-
         // set remote ip
         this._setIP(request);
 
@@ -190,6 +221,7 @@ class Peer {
         if (this.ip == '::1' || this.ip == '::ffff:127.0.0.1') {
             this.ip = '127.0.0.1';
         }
+        console.log("IP: " + this.ip)
     }
 
     _setPeerId(request) {
@@ -209,18 +241,18 @@ class Peer {
 
 
         let deviceName = '';
-        
+
         if (ua.os && ua.os.name) {
             deviceName = ua.os.name.replace('Mac OS', 'Mac') + ' ';
         }
-        
+
         if (ua.device.model) {
             deviceName += ua.device.model;
         } else {
             deviceName += ua.browser.name;
         }
 
-        if(!deviceName)
+        if (!deviceName)
             deviceName = 'Unknown Device';
 
         const displayName = uniqueNamesGenerator({
@@ -277,15 +309,15 @@ class Peer {
 }
 
 Object.defineProperty(String.prototype, 'hashCode', {
-  value: function() {
-    var hash = 0, i, chr;
-    for (i = 0; i < this.length; i++) {
-      chr   = this.charCodeAt(i);
-      hash  = ((hash << 5) - hash) + chr;
-      hash |= 0; // Convert to 32bit integer
+    value: function () {
+        var hash = 0, i, chr;
+        for (i = 0; i < this.length; i++) {
+            chr = this.charCodeAt(i);
+            hash = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
     }
-    return hash;
-  }
 });
 
-const server = new SnapdropServer(process.env.PORT || 3000);
+const server = new SnapdropServer(process.env.PORT || 3000, process.env.LAN || true);
